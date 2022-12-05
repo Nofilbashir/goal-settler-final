cab852742dd9ba9ad523b2a1cee60cd6922f75b1
Nofilbashir <nofil.bashiir@gmail.com>
2022-12-05 21:55:38 +0500

    final wala push

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use('/api/users', UserRouter)
 //serve Frontend
 
 if(process.env.NODE_ENV=== 'production'){
-    app.use(express.static(path.join(__dirname,'../frontend/build')))
-    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
+    app.use(express.static(path.join(__dirname,'./build')))
+    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname,'./','build','index.html')))
 }else{
     app.get('/',(req,res)=>res.send('server Running in Developemet Mode'))
 }
